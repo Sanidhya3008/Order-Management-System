89e3070e2aa9bb887a98cba0bc3f4c764a520b40
Sanidhya Manglunia <sanidhyamanglunia@gmail.com>
2024-10-15 22:24:48 +0530

    Updated server.js and api.js for base server URL

--- inventory-management-backend/server.js
+++ inventory-management-backend/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const User = require('./models/user');
 const bcrypt = require('bcryptjs');
+const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
@@ -18,6 +19,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../inventory-management-ui/build')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../inventory-management-ui/build', 'index.html'));
+    });
+}
 
 // Routes
 app.use('/api/auth', authRoutes);
